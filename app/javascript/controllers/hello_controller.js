dc4f302752879e4d241c1d67c98d2898d5264fca
Brian Allemeier <brian.all09@protonmail.com>
2022-08-16 18:54:09 +0100

    Use browser bundle

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -11,11 +11,7 @@ export default class extends Controller {
     event.preventDefault()
     console.log(solanaWeb3);
     const key = new solanaWeb3.PublicKey(this.inputTarget.value)
-    //const key = new Web3.PublicKey()
-    //console.log(`key: ${key}`)
     const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('devnet'))
-    //console.log(`connection: ${connection}`)
-
     connection.getBalance(key).then(balance => {
       console.log('balance:  ')
       console.log(balance / solanaWeb3.LAMPORTS_PER_SOL)
